34a4723861f01e3801c1ffa4c6b728bf49e33901
fairlaunchlabs <fairlaunchlabs@gmail.com>
2025-09-29 06:14:23 +0800

    flipflop-node 1.3.11

--- src/constants.ts
+++ src/constants.ts
@@ -77,7 +77,7 @@ export const TOKEN_PARAMS = {
     reduceRatio: new BN("75"),
     initialMintSize: new BN("20000000000000"),
     initialTargetMintSizePerEpoch: new BN("200000000000000"),
-    feeRate: new BN("50000000"),
+    feeRate: new BN("20000000"),
     liquidityTokensRatio: new BN("20"),
   }
 };
